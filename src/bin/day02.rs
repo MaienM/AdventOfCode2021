be7b33bc137b949de9e79641033c770d89c53ed2
Michon van Dooren <me@maienm.com>
2021-12-10 10:17:58 +0100

    Implement day02-2

--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -1,31 +1,74 @@
 use aoc::*;
 
-fn part1(input: String) -> String {
+#[derive(Debug, PartialEq)]
+enum Direction {
+    Forward,
+    Down,
+    Up,
+}
+
+type Instruction = (Direction, i32);
+
+fn parse_instructions(input: String) -> Vec<Instruction> {
     let lines = input.trim().split("\n").map(|l| l.trim());
+    return lines
+        .map(|line| {
+            let mut parts = line.splitn(2, " ");
+            let direction = parts
+                .next()
+                .unwrap_or_else(|| panic!("Unable to get direction from line {}.", line));
+            let direction = match direction {
+                "forward" => Direction::Forward,
+                "down" => Direction::Down,
+                "up" => Direction::Up,
+                _ => panic!("Invalid line {}.", line),
+            };
+            let distance = parts
+                .next()
+                .unwrap_or_else(|| panic!("Unable to get distance from line {}.", line))
+                .parse::<i32>()
+                .unwrap_or_else(|_| panic!("Unable to parse distance from line {}.", line));
+            return (direction, distance);
+        })
+        .collect();
+}
+
+fn part1(input: String) -> String {
+    let instructions = parse_instructions(input);
     let mut hpos = 0;
     let mut vpos = 0;
-    for line in lines {
-        let mut parts = line.splitn(2, " ");
-        let direction = parts
-            .next()
-            .unwrap_or_else(|| panic!("Unable to get direction from line {}.", line));
-        let distance = parts
-            .next()
-            .unwrap_or_else(|| panic!("Unable to get distance from line {}.", line))
-            .parse::<i32>()
-            .unwrap_or_else(|_| panic!("Unable to parse distance from line {}.", line));
+    for instruction in instructions {
+        let (direction, distance) = instruction;
         match direction {
-            "forward" => hpos += distance,
-            "down" => vpos += distance,
-            "up" => vpos -= distance,
-            _ => panic!("Invalid line {}.", line),
+            Direction::Forward => hpos += distance,
+            Direction::Down => vpos += distance,
+            Direction::Up => vpos -= distance,
+        }
+    }
+    return (hpos * vpos).to_string();
+}
+
+fn part2(input: String) -> String {
+    let instructions = parse_instructions(input);
+    let mut aim = 0;
+    let mut hpos = 0;
+    let mut vpos = 0;
+    for instruction in instructions {
+        let (direction, distance) = instruction;
+        match direction {
+            Direction::Forward => {
+                hpos += distance;
+                vpos += distance * aim;
+            }
+            Direction::Down => aim += distance,
+            Direction::Up => aim -= distance,
         }
     }
     return (hpos * vpos).to_string();
 }
 
 fn main() {
-    run(part1, missing);
+    run(part1, part2);
 }
 
 #[cfg(test)]
@@ -42,7 +85,26 @@ mod tests {
     ";
 
     #[test]
+    fn example_parse_instructions() {
+        let actual = parse_instructions(EXAMPLE_INPUT.to_string());
+        let expected = vec![
+            (Direction::Forward, 5),
+            (Direction::Down, 5),
+            (Direction::Forward, 8),
+            (Direction::Up, 3),
+            (Direction::Down, 8),
+            (Direction::Forward, 2),
+        ];
+        assert_eq!(&actual, &expected);
+    }
+
+    #[test]
     fn example_part1() {
         assert_eq!(part1(EXAMPLE_INPUT.to_string()), "150");
+    }
+
+    #[test]
+    fn example_part2() {
+        assert_eq!(part2(EXAMPLE_INPUT.to_string()), "900");
     }
 }
