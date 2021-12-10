8c54f5ae848c1aba11698888d5abd0cd5948c9b2
Michon van Dooren <me@maienm.com>
2021-12-10 15:30:42 +0100

    Implement day03-2

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -17,33 +17,85 @@ fn parse_grid_of_bits(input: String) -> Vec<Vec<i32>> {
         .collect();
 }
 
-fn part1(input: String) -> i32 {
-    let grid = parse_grid_of_bits(input);
+fn get_most_common_per_position(grid: &Vec<Vec<i32>>) -> Vec<i32> {
     let mut count_per_pos: Vec<[i32; 2]> = grid.first().unwrap().iter().map(|_| [0, 0]).collect();
-    let columns = count_per_pos.len();
     for line in grid {
         for (i, bit) in line.iter().enumerate() {
             count_per_pos[i][bit.to_owned() as usize] += 1;
         }
     }
+    return count_per_pos
+        .iter()
+        .map(|counts| {
+            if counts[0] > counts[1] {
+                return 0;
+            } else {
+                return 1;
+            }
+        })
+        .collect();
+}
 
-    let mut gamma = 0;
-    for counts in count_per_pos {
-        gamma = gamma << 1;
-        if counts[1] > counts[0] {
-            gamma += 1;
-        }
+fn bit_list_to_decimal(bits: &Vec<i32>) -> i32 {
+    let mut result = 0;
+    for bit in bits {
+        result = result << 1;
+        result += bit;
     }
+    return result;
+}
 
+fn part1(input: String) -> i32 {
+    let grid = parse_grid_of_bits(input);
+    let most_common_per_pos = get_most_common_per_position(&grid);
+
+    let gamma = bit_list_to_decimal(&most_common_per_pos);
     // Epsilon is really just gamma with all bits flipped, so just calculate it that way.
-    let filled_bits = (2 as i32).pow(columns as u32) - 1;
-    let epsilon = gamma ^ filled_bits;
+    let mask = (2 as i32).pow(most_common_per_pos.len() as u32) - 1;
+    let epsilon = gamma ^ mask;
 
     return gamma * epsilon;
 }
 
+fn part2(input: String) -> i32 {
+    let grid = parse_grid_of_bits(input);
+    let columns = grid.first().unwrap().clone().len();
+
+    let mut oxygen_candidates = grid.iter().cloned().collect();
+    let mut scrubber_candidates = grid;
+
+    for i in 0..columns {
+        let oxygen_most_common = get_most_common_per_position(&oxygen_candidates);
+        let oxygen_criteria = oxygen_most_common[i];
+        oxygen_candidates = oxygen_candidates
+            .into_iter()
+            .filter(|bits| bits[i] == oxygen_criteria)
+            .collect();
+        if oxygen_candidates.len() == 1 {
+            break;
+        }
+    }
+
+    for i in 0..columns {
+        let scrubber_most_common = get_most_common_per_position(&scrubber_candidates);
+        let scrubber_criteria = 1 - scrubber_most_common[i];
+        scrubber_candidates = scrubber_candidates
+            .into_iter()
+            .filter(|bits| bits[i] == scrubber_criteria)
+            .collect();
+        if scrubber_candidates.len() == 1 {
+            break;
+        }
+    }
+
+    let oxygen = bit_list_to_decimal(oxygen_candidates.first().unwrap());
+    let scrubber = bit_list_to_decimal(scrubber_candidates.first().unwrap());
+
+    return oxygen * scrubber;
+}
+
 fn main() {
-    run(part1, missing);
+    run(part1, part2);
 }
 
 #[cfg(test)]
@@ -68,5 +120,10 @@ mod tests {
     #[test]
     fn example_part1() {
         assert_eq!(part1(EXAMPLE_INPUT.to_string()), 198);
+    }
+
+    #[test]
+    fn example_part2() {
+        assert_eq!(part2(EXAMPLE_INPUT.to_string()), 230);
     }
 }
