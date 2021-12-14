b204d3a3c6251a5442bfd726ac74b0c0c2f1d0ff
Michon van Dooren <me@maienm.com>
2021-12-14 13:43:37 +0100

    Improve runner output with colours

--- src/runner.rs
+++ src/runner.rs
@@ -1,3 +1,5 @@
+use ansi_term::Colour;
+use ansi_term::Colour::*;
 use std::env;
 use std::fs;
 use std::time::Instant;
@@ -14,12 +16,24 @@ fn do_runnable<T: ToString>(name: &'static str, runnable: Runnable<T>, input: &S
     let result = runnable(input.to_owned());
     let duration = start.elapsed();
 
+    let duration_colour: Colour = if duration.as_secs() > 0 {
+        Red
+    } else if duration.as_millis() > 0 {
+        Blue
+    } else {
+        Green
+    };
+    let duration_formatted = duration_colour.paint(format!("{:?}", duration));
+
+    let name = Purple.paint(name);
     let result = result.to_string();
     if result.contains("\n") {
-        println!("> {}: [{:?}]", name, duration);
-        println!("{}", result);
+        println!("> {}: [{}]", name, duration_formatted);
+        for line in result.split("\n") {
+            println!("  {}", line);
+        }
     } else {
-        println!("> {}: {} [{:?}]", name, result, duration);
+        println!("> {}: {} [{}]", name, result, duration_formatted);
     }
 }
 
@@ -34,7 +48,11 @@ pub fn run<T1: ToString, T2: ToString>(part1: Runnable<T1>, part2: Runnable<T2>)
     let filename = format!("inputs/{}.txt", name);
     let filename = args.get(1).unwrap_or(&filename);
 
-    println!("Running {} with input {}...", name, filename);
+    println!(
+        "Running {} with input '{}'...",
+        Cyan.paint(name),
+        Cyan.paint(filename)
+    );
 
     let input = fs::read_to_string(filename).expect("Unable to read input file.");
     do_runnable("Part 1", part1, &input);
