947496bd291f6125f273735692381892564879bb
Michon van Dooren <me@maienm.com>
2021-12-18 17:08:35 +0100

    Implement day18-2

--- src/bin/day18.rs
+++ src/bin/day18.rs
@@ -2,7 +2,7 @@ use std::{collections::VecDeque, fmt::Debug, iter::Peekable, str::Chars};
 
 use aoc::runner::*;
 
-#[derive(Eq, PartialEq)]
+#[derive(Clone, Eq, PartialEq)]
 struct Pair(Item, Item);
 impl Debug for Pair {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -10,7 +10,7 @@ impl Debug for Pair {
     }
 }
 
-#[derive(Eq, PartialEq)]
+#[derive(Clone, Eq, PartialEq)]
 enum Item {
     Pair(Box<Pair>),
     Num(i8),
@@ -260,8 +260,26 @@ fn part1(input: String) -> i32 {
     return get_magnitude(&result);
 }
 
+fn part2(input: String) -> i32 {
+    let pairs = parse_input(input);
+    let mut highest = 0;
+    for pair in &pairs {
+        for pair2 in &pairs {
+            if pair == pair2 {
+                continue;
+            }
+
+            let result = get_magnitude(&sum(pair.clone(), pair2.clone()));
+            if result > highest {
+                highest = result;
+            }
+        }
+    }
+    return highest;
+}
+
 fn main() {
-    run(part1, missing::<i8>);
+    run(part1, part2);
 }
 
 #[cfg(test)]
@@ -1069,25 +1087,26 @@ mod tests {
         );
     }
 
+    const EXAMPLE_INPUT: &'static str = "
+        [[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
+        [[[5,[2,8]],4],[5,[[9,9],0]]]
+        [6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
+        [[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]
+        [[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]
+        [[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]
+        [[[[5,4],[7,7]],8],[[8,3],8]]
+        [[9,3],[[9,9],[6,[4,9]]]]
+        [[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
+        [[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]
+    ";
+
     #[test]
     fn example_part1() {
-        assert_eq!(
-            part1(
-                "
-                    [[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
-                    [[[5,[2,8]],4],[5,[[9,9],0]]]
-                    [6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
-                    [[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]
-                    [[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]
-                    [[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]
-                    [[[[5,4],[7,7]],8],[[8,3],8]]
-                    [[9,3],[[9,9],[6,[4,9]]]]
-                    [[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
-                    [[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]
-                "
-                .to_string()
-            ),
-            4140
-        );
+        assert_eq!(part1(EXAMPLE_INPUT.to_string()), 4140);
+    }
+
+    #[test]
+    fn example_part2() {
+        assert_eq!(part2(EXAMPLE_INPUT.to_string()), 3993);
     }
 }
