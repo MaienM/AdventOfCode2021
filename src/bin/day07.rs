0d4ad386f18117c3af6b7683e7eaa3d976205b7b
Michon van Dooren <me@maienm.com>
2021-12-21 23:02:58 +0100

    Fix correctness issue in day07-1
    The solution for this was based on an assumption that just happened to
    hold for the example and my input, but which wasn't valid in all cases.

--- src/bin/day07.rs
+++ src/bin/day07.rs
@@ -1,15 +1,13 @@
 use aoc::{parse_number_list, runner::*};
 
-fn mean(numbers: &Vec<i32>) -> i32 {
-    let mut sorted = numbers.clone();
-    sorted.sort();
-    let len = sorted.len();
-    if len % 2 == 0 {
-        let avg = f64::from(sorted[len / 2 - 1] + sorted[len / 2]) / 2.0;
-        return avg.round() as i32;
-    } else {
-        return sorted[len / 2];
-    }
+fn get_cost_linear(numbers: &Vec<i32>, target: i32) -> i32 {
+    return numbers
+        .iter()
+        .map(|p| {
+            return (target - p).abs();
+        })
+        .sum::<i32>()
+        .into();
 }
 
 fn get_cost_exponential(numbers: &Vec<i32>, target: i32) -> i32 {
@@ -23,19 +21,7 @@ fn get_cost_exponential(numbers: &Vec<i32>, target: i32) -> i32 {
         .into();
 }
 
-pub fn part1(input: String) -> i64 {
-    let positions = parse_number_list(input, ",");
-    let target = mean(&positions);
-    return positions
-        .iter()
-        .map(|p| (target - p).abs())
-        .sum::<i32>()
-        .into();
-}
-
-pub fn part2(input: String) -> i64 {
-    let positions = parse_number_list(input, ",");
-
+fn find_optimum(positions: &Vec<i32>, get_cost: fn(&Vec<i32>, i32) -> i32) -> i32 {
     // This function assumes a distribution where there is a steady increase in cost when moving away from the optimum result.
 
     // Start with a (sort of) binary search, to get close to the optimum result as quickly as possible.
@@ -50,8 +36,8 @@ pub fn part2(input: String) -> i64 {
         }
         let new_targets = (target - level_size, target + level_size);
         let new_costs = (
-            get_cost_exponential(&positions, new_targets.0),
-            get_cost_exponential(&positions, new_targets.1),
+            get_cost(&positions, new_targets.0),
+            get_cost(&positions, new_targets.1),
         );
         if new_costs.0 > new_costs.1 {
             target = new_targets.1;
@@ -61,15 +47,15 @@ pub fn part2(input: String) -> i64 {
     }
 
     // Target should now be close, but might be not quite there. Figure out if one of the directions is an improvement, and if so keep moving in that direction until results become worse.
-    let mut cost = get_cost_exponential(&positions, target);
-    let direction = if get_cost_exponential(&positions, target - 1) < cost {
+    let mut cost = get_cost(&positions, target);
+    let direction = if get_cost(&positions, target - 1) < cost {
         -1
     } else {
         1
     };
 
     loop {
-        let new_cost = get_cost_exponential(&positions, target + direction);
+        let new_cost = get_cost(&positions, target + direction);
         if new_cost > cost {
             return cost.into();
         }
@@ -77,6 +63,16 @@ pub fn part2(input: String) -> i64 {
         target += direction;
         cost = new_cost;
     }
+}
+
+pub fn part1(input: String) -> i32 {
+    let positions = parse_number_list(input, ",");
+    return find_optimum(&positions, get_cost_linear);
+}
+
+pub fn part2(input: String) -> i32 {
+    let positions = parse_number_list(input, ",");
+    return find_optimum(&positions, get_cost_exponential);
 }
 
 fn main() {
